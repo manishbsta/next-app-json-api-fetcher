8dfe6df3b529089b5ad2bc49027d4ece12c8e11e
manishbsta <manishbsta@gmail.com>
2021-05-08 11:46:47 +0545

    meta keywords desc change

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -4,7 +4,7 @@ const MetaHead = ({title, description}) => {
   return (
     <Head>
       <title>codeWithManish {title && `| ${title}` }</title>
-      <meta name="keywords" content="manish" />
+      <meta name="keywords" content="manish, codeWithManish, nextjs" />
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.png" type="image/*" />
     </Head>
